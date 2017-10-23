bbf8f6967f8ac469435354b43193a5030ba43838
Giulio Petek <gi-lo@touch-mania.com>
2017-10-23 17:46:55 +0200

    Update to use new version

---
+++ sample/src/main/java/de/rheinfabrik/heimdalldroid/TraktTvAPIConfiguration.java
@@ -0,0 +1,7 @@
+package de.rheinfabrik.heimdalldroid;
+
+public class TraktTvAPIConfiguration {
+    public static final String CLIENT_ID = "***REMOVED***";
+    public static final String CLIENT_SECRET = "***REMOVED***";
+    public static final String REDIRECT_URI = "***REMOVED***";
+}
