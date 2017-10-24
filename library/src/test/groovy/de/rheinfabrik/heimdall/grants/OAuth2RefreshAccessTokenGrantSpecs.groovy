65a59a23ce0c12c8537b39c8d46022eb11c36871
Giulio Petek <gi-lo@touch-mania.com>
2017-10-24 09:13:36 +0200

    Move tests

--- library/src/test/groovy/de/rheinfabrik/heimdall/grants/OAuth2RefreshAccessTokenGrantSpecs.groovy
+++ library/src/test/groovy/de/rheinfabrik/heimdall/grants/OAuth2RefreshAccessTokenGrantSpecs.groovy
@@ -1,10 +1,10 @@
 package de.rheinfabrik.heimdall.grants
 
-import com.andrewreitz.spock.android.AndroidSpecification
+import spock.lang.Specification
 import spock.lang.Title
 
 @Title("Specs for the OAuth2RefreshAccessTokenGrant")
-class OAuth2RefreshAccessTokenGrantSpecs extends AndroidSpecification {
+class OAuth2RefreshAccessTokenGrantSpecs extends Specification {
 
     // Scenarios
 
