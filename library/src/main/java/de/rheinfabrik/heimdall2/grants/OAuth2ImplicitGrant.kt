cbf44aaac2c1b3e92384b543193b103f9f0e2690
Walter Ching <walter.ching.b@gmail.com>
2019-05-21 18:31:45 +0200

    Migrate OAuth2ImplicitGrant.kt

--- library/src/main/java/de/rheinfabrik/heimdall2/grants/OAuth2ImplicitGrant.kt
+++ library/src/main/java/de/rheinfabrik/heimdall2/grants/OAuth2ImplicitGrant.kt
@@ -1,41 +1,31 @@
-package de.rheinfabrik.heimdall2.grants;
+package de.rheinfabrik.heimdall2.grants
 
-import de.rheinfabrik.heimdall2.OAuth2AccessToken;
+import de.rheinfabrik.heimdall2.OAuth2AccessToken
 
 /**
  * Class representing the Implicit Grant as described in https://tools.ietf.org/html/rfc6749#section-4.2.
  *
  * @param <TAccessToken> The access token type.
  */
-public abstract class OAuth2ImplicitGrant<TAccessToken extends OAuth2AccessToken> implements OAuth2Grant<TAccessToken> {
 
-    // Constants
-
-    /**
-     * REQUIRED
-     * The "response_type" which MUST be "token".
-     */
-    public final static String RESPONSE_TYPE = "token";
-
-    // Properties
-
+abstract class OAuth2ImplicitGrant<TAccessToken : OAuth2AccessToken>(
     /**
      * REQUIRED
      * The client identifier as described in https://tools.ietf.org/html/rfc6749#section-2.2.
      */
-    public String clientId;
+    var clientId: String = "",
 
     /**
      * OPTIONAL
      * As described in https://tools.ietf.org/html/rfc6749#section-3.1.2.
      */
-    public String redirectUri;
+    var redirectUri: String? = null,
 
     /**
      * OPTIONAL
      * The scope of the access request as described in https://tools.ietf.org/html/rfc6749#section-3.3.
      */
-    public String scope;
+    var scope: String? = null,
 
     /**
      * RECOMMENDED
@@ -45,5 +35,18 @@ public abstract class OAuth2ImplicitGrant<TAccessToken extends OAuth2AccessToken
      * to the client. The parameter SHOULD be used for preventing
      * cross-site request forgery as described in https://tools.ietf.org/html/rfc6749#section-10.12.
      */
-    public String state;
+    var state: String? = null
+
+) : OAuth2Grant<TAccessToken> {
+
+    // Constants
+
+    /**
+     * REQUIRED
+     * The "response_type" which MUST be "token".
+     */
+    companion object {
+        @JvmStatic
+        val RESPONSE_TYPE = "token"
+    }
 }
