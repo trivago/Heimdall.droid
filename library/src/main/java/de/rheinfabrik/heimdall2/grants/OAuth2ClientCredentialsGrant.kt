f8f0781a1a59e8d99d55abbed6009a62fc772c93
Walter Ching <walter.ching.b@gmail.com>
2019-05-21 18:20:24 +0200

    Migrate OAuth2ClientCredentialsGrant
    Create .kt file
    Delete .java file

---
+++ library/src/main/java/de/rheinfabrik/heimdall2/grants/OAuth2ClientCredentialsGrant.kt
@@ -0,0 +1,24 @@
+package de.rheinfabrik.heimdall2.grants
+
+import de.rheinfabrik.heimdall2.OAuth2AccessToken
+
+abstract class OAuth2ClientCredentialsGrant<TAccessToken : OAuth2AccessToken>(
+    /**
+     * OPTIONAL
+     * The scope of the access request as described in https://tools.ietf.org/html/rfc6749#section-3.3.
+     */
+    val scope: String? = null
+) :
+    OAuth2Grant<TAccessToken> {
+
+    // Constants
+
+    companion object {
+        /**
+         * REQUIRED
+         * The OAuth2 "grant_type".
+         */
+        @JvmStatic
+        val GRANT_TYPE = "client_credentials"
+    }
+}
