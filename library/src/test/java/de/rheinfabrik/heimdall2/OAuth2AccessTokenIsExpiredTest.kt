350d8c94a31f4ecd4e21ef91531e88f7c7e7e416
Walter Ching <walter.ching.b@gmail.com>
2019-07-15 16:54:38 +0200

    Add OAuth2AccessToken JUnit tests

---
+++ library/src/test/java/de/rheinfabrik/heimdall2/OAuth2AccessTokenIsExpiredTest.kt
@@ -0,0 +1,53 @@
+package de.rheinfabrik.heimdall2
+
+import org.junit.Assert.assertEquals
+import org.junit.Test
+import java.util.*
+
+class OAuth2AccessTokenIsExpiredTest {
+
+    @Test
+    fun `when the expirationDate is null, isExpired method should return false`() {
+        // given an accessToken
+        val accessToken = OAuth2AccessToken(expirationDate = null)
+
+        // when the isExpired method is called
+        val value = accessToken.isExpired()
+
+        // then false is returned
+        assertEquals(value, false)
+
+    }
+
+    @Test
+    fun `when the expirationDate is in the past, isExpired method should return false`() {
+        // given a date in the past
+        val pastCalendar = Calendar.getInstance()
+        pastCalendar.add(Calendar.YEAR, 1)
+
+        // and a token with a past date
+        val accessToken = OAuth2AccessToken(expirationDate = pastCalendar)
+
+        // when the isExpired method is called
+        val value = accessToken.isExpired()
+
+        // then true is returned
+        assertEquals(value, false)
+    }
+
+    @Test
+    fun `when the expirationDate is in the future, isExpired method should return false`() {
+        // given a date in the past
+        val futureDate = Calendar.getInstance()
+        futureDate.add(Calendar.YEAR, 1)
+
+        // and a token with a past date
+        val accessToken = OAuth2AccessToken(expirationDate = futureDate)
+
+        // when the isExpired method is called
+        val value = accessToken.isExpired()
+
+        // then true is returned
+        assertEquals(value, false)
+    }
+}
---
+++ library/src/test/java/de/rheinfabrik/heimdall2/OAuth2AccessTokenManagerTest.kt
@@ -0,0 +1,32 @@
+package de.rheinfabrik.heimdall2
+
+import com.nhaarman.mockitokotlin2.mock
+import com.nhaarman.mockitokotlin2.whenever
+import de.rheinfabrik.heimdall2.grants.OAuth2RefreshAccessTokenGrant
+import io.reactivex.Single
+import org.junit.Test
+
+
+class OAuth2AccessTokenManagerTest {
+
+    @Test
+    fun `when the access token is accessed, the token manager should emit a non expired token`(){
+        // given a stored access token
+        val validToken = mock<OAuth2AccessToken>()
+        val accessTokenStorage = mock<OAuth2AccessTokenStorage>()
+        whenever(accessTokenStorage.getStoredAccessToken()).thenReturn(Single.just(validToken))
+
+        // and a refresh access token grant
+        val refreshAccessTokenGrant = mock<OAuth2RefreshAccessTokenGrant>()
+        whenever(refreshAccessTokenGrant.grantNewAccessToken()).thenReturn(Single.just(validToken))
+
+        // and a token manager
+        val tokenManager = OAuth2AccessTokenManager(accessTokenStorage)
+
+        // when the token is accessed
+        val getValidAccessTokenTest = tokenManager.getValidAccessToken(refreshAccessTokenGrant).test()
+
+        // then the token is received
+        getValidAccessTokenTest.assertValue(validToken)
+    }
+}
