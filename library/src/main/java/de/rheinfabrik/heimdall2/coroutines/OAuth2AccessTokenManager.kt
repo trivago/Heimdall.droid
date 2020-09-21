43640c350ae905e137816f2670fd1873a7f98448
Walter Ching <walter.ching.b@gmail.com>
2020-09-21 22:41:29 +0200

    Corroutine base access token manager

---
+++ library/src/main/java/de/rheinfabrik/heimdall2/coroutines/OAuth2AccessTokenManager.kt
@@ -0,0 +1,67 @@
+package de.rheinfabrik.heimdall2.coroutines
+
+import de.rheinfabrik.heimdall2.coroutines.grants.OAuth2Grant
+import de.rheinfabrik.heimdall2.coroutines.grants.OAuth2RefreshAccessTokenGrant
+import de.rheinfabrik.heimdall2.model.OAuth2AccessToken
+import java.util.*
+
+open class OAuth2AccessTokenManager(
+    private val mStorage: OAuth2AccessTokenStorage
+) {
+
+    // Public API
+
+    /**
+     * Returns the underlying storage.
+     *
+     * @return - An OAuth2AccessTokenStorage.
+     */
+    fun getStorage(): OAuth2AccessTokenStorage = mStorage
+
+    /**
+     * Grants a new access token using the given OAuth2 grant.
+     *
+     * @param grant A class implementing the OAuth2Grant interface.
+     * @return - A granted access token.
+     */
+    suspend fun grantNewAccessToken(
+        grant: OAuth2Grant,
+        calendar: Calendar = Calendar.getInstance()
+    ): OAuth2AccessToken {
+        val accessToken = grant.grantNewAccessToken()
+        val newAccessToken = if (accessToken.expiresIn != null) {
+            val newExpirationDate = (calendar.clone() as Calendar).apply {
+                add(Calendar.SECOND, accessToken.expiresIn)
+            }
+            accessToken.copy(expirationDate = newExpirationDate)
+        } else {
+            accessToken
+        }
+
+        mStorage.storeAccessToken(
+            token = newAccessToken
+        )
+        return newAccessToken
+    }
+
+    /**
+     * Returns an Observable emitting an unexpired access token.
+     * NOTE: In order to work, Heimdall needs an access token which has a refresh_token and an
+     * expires_in field.
+     *
+     * @param refreshAccessTokenGrant The refresh grant that will be used if the access token is expired.
+     * @return - An unexpired access token.
+     */
+    suspend fun getValidAccessToken(refreshAccessTokenGrant: OAuth2RefreshAccessTokenGrant): OAuth2AccessToken {
+        val accessToken = mStorage.getStoredAccessToken()
+
+        return if (accessToken.isExpired()) {
+            refreshAccessTokenGrant.refreshToken = accessToken.refreshToken
+            grantNewAccessToken(
+                grant = refreshAccessTokenGrant
+            )
+        } else {
+            accessToken
+        }
+    }
+}
