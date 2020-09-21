ec1f8264a1e47c9af18ee2aae9d7fe2671230729
Walter Ching <walter.ching.b@gmail.com>
2020-09-21 22:38:38 +0200

    Create new OAuth2Grant with suspend

---
+++ library/src/main/java/de/rheinfabrik/heimdall2/coroutines/grants/OAuth2Grant.kt
@@ -0,0 +1,21 @@
+package de.rheinfabrik.heimdall2.coroutines.grants
+
+import de.rheinfabrik.heimdall2.model.OAuth2AccessToken
+import io.reactivex.Single
+
+/**
+ * Interface describing an OAuth2 Grant as described in https://tools.ietf.org/html/rfc6749#page-23.
+ *
+ * @param <TAccessToken> The access token type.
+ */
+interface OAuth2Grant {
+
+    // Abstract Api
+
+    /**
+     * Performs the actual request to grant a new access token.
+     *
+     * @return - The granted access token.
+     */
+    suspend fun grantNewAccessToken(): OAuth2AccessToken
+}
