25e4dc6644260e062057fc44929b1a32b7c35cf0
Walter Ching <walter.ching.b@gmail.com>
2020-09-29 23:32:49 +0200

    Add simple grants based on suspendable oauth2grant

---
+++ library/src/main/java/de/rheinfabrik/heimdall2/coroutines/grants/OAuth2ResourceOwnerPasswordCredentialsGrant.kt
@@ -0,0 +1,26 @@
+package de.rheinfabrik.heimdall2.coroutines.grants
+
+abstract class OAuth2ResourceOwnerPasswordCredentialsGrant(
+    /**
+     * REQUIRED
+     * The resource owner "username".
+     */
+    var username: String = "",
+
+    /**
+     * REQUIRED
+     * The resource owner "password".
+     */
+    var password: String = "",
+
+    /**
+     * OPTIONAL
+     * The "scope" of the access request as described by here (https://tools.ietf.org/html/rfc6749#section-3.3).
+     */
+    var scope: String? = null
+) : OAuth2Grant {
+
+    companion object {
+        const val GRANT_TYPE = "password"
+    }
+}
