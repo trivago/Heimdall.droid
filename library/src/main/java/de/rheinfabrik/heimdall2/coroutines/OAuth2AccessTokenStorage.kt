568f5509ea10da0cc1fc844813aaad9251e32860
Walter Ching <walter.ching.b@gmail.com>
2020-10-04 22:06:41 +0200

    Create suspendable access token storage interface

---
+++ library/src/main/java/de/rheinfabrik/heimdall2/coroutines/OAuth2AccessTokenStorage.kt
@@ -0,0 +1,39 @@
+package de.rheinfabrik.heimdall2.coroutines
+
+import de.rheinfabrik.heimdall2.accesstoken.OAuth2AccessToken
+
+/**
+ * Interface used to define how to store and retrieve a stored access token.
+ *
+ * @param <TAccessToken> The access token type.
+ */
+interface OAuth2AccessTokenStorage {
+    // Public API
+
+    /**
+     * Queries the stored access token.
+     *
+     * @return - An Observable emitting the stored access token.
+     */
+    suspend fun getStoredAccessToken(): OAuth2AccessToken
+
+    /**
+     * Stores the given access token.
+     *
+     * @param token The access token which will be stored.
+     */
+    suspend fun storeAccessToken(token: OAuth2AccessToken)
+
+    /**
+     * Checks whether there is or is not an access token
+     *
+     * @return - An Observable emitting true or false based on whether there is or is not an
+     * access token.
+     */
+    suspend fun hasAccessToken(): Boolean
+
+    /**
+     * Removes the stored access token.
+     */
+    suspend fun removeAccessToken()
+}
